0a9752de39cfc62d854ea75e035af9780cb39d5c
limard <limard@gmail.com>
2020-10-21 15:09:16 +0800

    修改import

--- main.go
+++ main.go
@@ -5,30 +5,31 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
-	"mp4info/mov"
+
+	"github.com/Limard/mp4info/mov"
 )
 
 func main() {
 	fmt.Println("vim-go")
-	pfile , err := os.Open("F:\\video\\file6.mp4")
-	if err != nil{
+	pfile, err := os.Open("F:\\video\\file6.mp4")
+	if err != nil {
 		log.Panic(err.Error())
 		return
 	}
 
-	buf , err := ioutil.ReadAll(pfile)
-	if err != nil{
+	buf, err := ioutil.ReadAll(pfile)
+	if err != nil {
 		log.Panic(err.Error())
 		return
 	}
 
 	for {
-		boxLen , err := mov.ParseBox(buf)
-		if err != nil{
+		boxLen, err := mov.ParseBox(buf)
+		if err != nil {
 			log.Println(err.Error())
 		}
 		buf = buf[boxLen:]
-		if len(buf) == 0{
+		if len(buf) == 0 {
 			break
 		}
 	}
